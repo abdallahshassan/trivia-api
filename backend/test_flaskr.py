3ca55ccc5984d18770f62b755689419ccaebac4e
Abdallah S. Hassan <abdallahaminm@gmail.com>
2020-08-24 11:31:14 +0200

    remove usused imports

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,9 +1,8 @@
-import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import setup_db, Question
 
 
 class TriviaTestCase(unittest.TestCase):
